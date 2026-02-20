129dd609522910b94700c2981fdbdc1f3ead485f
Saurav Gurhale <sauravlaxmikant3010@gmail.com>
2026-02-20 09:14:39 -0500

    small updates

--- api/supabase.py
+++ api/supabase.py
@@ -1,8 +1,8 @@
 import os
-from functools import lru_cache
 
-from supabase import Client, create_client
+from supabase import create_client
 from pydantic import BaseModel
+
 
 def get_env(name: str) -> str:
     value = os.getenv(name)
@@ -11,16 +11,21 @@ def get_env(name: str) -> str:
     return value
 
 
-
 class Supabase(BaseModel):
     def __init__(self):
         url = get_env("SUPABASE_URL")
         key = get_env("SUPABASE_SERVICE_ROLE_KEY")
 
         self._client = create_client(url, key)
-    
-    def get_client(self) -> Client:
-        return self._client
 
     def get_co_audio(self, practice_id: str) -> None:
+        """For a given practice_id, get the full audio for that practice"""
+        # returns the audio file?
+        pass
+
+    def get_co_practice_id(title: str) -> str:
+        """Get the practice_id for a given co title"""
+        pass
+
+    def post_co_audio_segments(self) -> None:
         pass
